1b5947e63b2d1ab33106c441e5e7cce4aaef2958
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-22 09:52:30 -0400

    Added scanner

--- src/main/java/com/pluralsight/PayrollReader.java
+++ src/main/java/com/pluralsight/PayrollReader.java
@@ -2,9 +2,16 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class PayrollReader {
     public static void main(String[] args) {
+
+        //Getting user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter employee name (First Last): ");
+        String empName = scanner.nextLine();
+
 
         //Reading the file
         FileReader fileReader = new FileReader("src/main/resources/employees.csv");
