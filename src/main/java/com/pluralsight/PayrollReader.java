73a38bd209c1a4a7d3b6c8f9c2e6ac4d1c8007fa
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-22 10:18:49 -0400

    Added try/catch & exceptions

--- src/main/java/com/pluralsight/PayrollReader.java
+++ src/main/java/com/pluralsight/PayrollReader.java
@@ -2,36 +2,45 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PayrollReader {
     public static void main(String[] args) {
+        try {
+            //getting use input
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter employee name (First Last): ");
+            String empName = scanner.nextLine();
 
-        //Getting user input
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter employee name (First Last): ");
-        String empName = scanner.nextLine();
+            //reading file
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader buffreader = new BufferedReader(fileReader);
 
+            String line;
 
-        //Reading the file
-        FileReader fileReader = new FileReader("src/main/resources/employees.csv");
-        BufferedReader buffreader = new BufferedReader(fileReader);
+            while ((line = buffreader.readLine()) != null) {
+                System.out.println(line);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
 
-        String line;
+                }
+            }
 
-        while ((line = buffreader.readLine()) != null) {
-
-        }
-
-
-
-
-
-
-
-
-
-
-
+            buffreader.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }// close curly for initial try
     }
-}
+    }
+
+
+
+
+
+
+
+
+
