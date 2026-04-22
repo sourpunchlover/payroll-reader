2f63b7683c4a61dc6748e7b725ee829887aa7627
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-22 09:46:53 -0400

    Added file & buff readers & while loop

--- src/main/java/com/pluralsight/PayrollReader.java
+++ src/main/java/com/pluralsight/PayrollReader.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class PayrollReader {
+    public static void main(String[] args) {
+
+        //Reading the file
+        FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+        BufferedReader buffreader = new BufferedReader(fileReader);
+
+        String line;
+
+        while ((line = buffreader.readLine()) != null) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
